70e610ef16fe2ec2b702d431d89621b7f1ed195b
Rustam <ruslivaev05@gmail.com>
2024-01-03 23:12:40 +0600

    add cartItems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 function App() {
 	return (
 		<div className='wrapper clear'>
+			<div className='overlay'>
+				<div className='drawer'>
+					<h2 className='mb-30'>Корзина</h2>
+					<div className='cartItem d-flex align-center mb-20'>
+						<div
+							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+							className='cartItemImg'></div>
+
+						<div className='mr-20 flex'>
+							<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+							<b>12 999 руб.</b>
+						</div>
+						<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
+					</div>
+					<div className='cartItem d-flex align-center'>
+						<div
+							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+							className='cartItemImg'></div>
+
+						<div className='mr-20 flex'>
+							<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+							<b>12 999 руб.</b>
+						</div>
+						<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
+					</div>
+				</div>
+			</div>
 			<header className='d-flex justify-between align-center p-40'>
 				<div className='d-flex align-center'>
 					<img width={40} height={40} src='/img/logo.png' alt='logo' />
