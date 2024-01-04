3d2be988a1dbfaa7ada177008b5e4102f685a715
Rustam <ruslivaev05@gmail.com>
2024-01-04 19:44:09 +0600

    add button in cart

--- src/App.js
+++ src/App.js
@@ -4,27 +4,44 @@ function App() {
 			<div className='overlay'>
 				<div className='drawer'>
 					<h2 className='mb-30'>Корзина</h2>
-					<div className='cartItem d-flex align-center mb-20'>
-						<div
-							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-							className='cartItemImg'></div>
+					<div className='items'>
+						<div className='cartItem d-flex align-center mb-20'>
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+								className='cartItemImg'></div>
 
-						<div className='mr-20 flex'>
-							<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
-							<b>12 999 руб.</b>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
 						</div>
-						<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
+						<div className='cartItem d-flex align-center'>
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }}
+								className='cartItemImg'></div>
+
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
+						</div>
 					</div>
-					<div className='cartItem d-flex align-center'>
-						<div
-							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-							className='cartItemImg'></div>
-
-						<div className='mr-20 flex'>
-							<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
-							<b>12 999 руб.</b>
-						</div>
-						<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
+					<div className='cartTotalBlock'>
+						<ul>
+							<li>
+								<span>Итого:</span>
+								<div></div>
+								<b>21 498 руб.</b>
+							</li>
+							<li>
+								<span>Налог 5%:</span>
+								<div></div>
+								<b>1074 руб.</b>
+							</li>
+						</ul>
+						<button>Оформить заказ</button>
 					</div>
 				</div>
 			</div>
