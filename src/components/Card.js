347f820d307b328a56ddaaa6550dd5f97c3e9984
Rustam <ruslivaev05@gmail.com>
2024-01-11 23:06:50 +0600

    add props in cart compopnent

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-export const Card = () => {
+export const Card = (props) => {
 	return (
 		<div className='card'>
 			<div className='favorite'>
 				<img src='/img/heard-unliked.svg' alt='Unliked' />
 			</div>
 			<img width={133} height={112} src='/img/sneakers/1.jpg' alt='' />
-			<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+			<h5>{props.title}</h5>
 			<div className='d-flex justify-between align-center'>
 				<div className='d-flex flex-column'>
 					<span>Цена:</span>
