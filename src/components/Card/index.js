9f9e7e5ee45ecc95281864fcd2ea69cdd3675949
Rustam <ruslivaev05@gmail.com>
2024-01-14 01:30:26 +0600

    change css on scss modules

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,9 @@
+import styles from './Card.module.scss';
+
 export const Card = (props) => {
 	return (
-		<div className='card'>
-			<div className='favorite'>
+		<div className={styles.card}>
+			<div className={styles.favorite}>
 				<img src='/img/heard-unliked.svg' alt='Unliked' />
 			</div>
 			<img width={133} height={112} src={props.imagesUrl} alt='' />
